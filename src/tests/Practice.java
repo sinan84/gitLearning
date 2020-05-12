16ac2d88d1747a3e3a7e48cef22ebe8bf5a458b8
sinan84 <sinanabasiyanik@gmail.com>
2020-05-12 15:46:18 +0100

    add Practice.java

---
+++ src/tests/Practice.java
@@ -0,0 +1,17 @@
+package tests;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+            String str="sinan";
+
+            int num1=111;
+            int num2=1212;
+
+
+
+
+        }
+    }
+
